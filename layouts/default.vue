a03e3a866d0bf8282d32472cb8a6e75d47252fcb
DosDima <dosdima89@gmail.com>
2024-10-23 20:50:02 +0300

    projet sructure

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout">
+    <slot />
+    <EffectCircles/>
+  </div>
+</template>
+
+<style scoped>
+.layout{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+</style>
